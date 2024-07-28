d121cf70d1de030ba3d1a931bed873f425da4006
manas-vyas <66221115+manas-vyas@users.noreply.github.com>
2024-07-28 14:30:58 +0530

    Update EmailNotification.groovy

--- vars/EmailNotification.groovy
+++ vars/EmailNotification.groovy
@@ -1,6 +1,6 @@
 def call() {
   // emailext subject: "${currentBuild.currentResult}: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}", attachLog: true, attachmentsPattern: '*.log', body: """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p><br>""", mimeType: 'text/html', recipientProviders: [requestor()], to: ''
 
-  emailext (body: '''${SCRIPT, template="groovy-html-1.template"}''', mimeType: 'text/html', subject: "${currentBuild.currentResult}: Build ${env.JOB_NAME} - ${env.BUILD_NUMBER}", attachLog: false, recipientProviders: [requestor()], to: "manashwiv@epiqinfo.com")
+  emailext (body: '''${SCRIPT, template="groovy-html-1.template"}''', mimeType: 'text/html', subject: "${currentBuild.currentResult}: Build ${env.JOB_NAME} - ${env.BUILD_NUMBER}", attachLog: false, recipientProviders: [requestor()], to: "manas.vyas29@gmail.com")
 
 }
